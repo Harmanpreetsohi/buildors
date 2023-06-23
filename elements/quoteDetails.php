adb6cf3048548257a97b69533623d4d3b11045b3
harmanmailto:harmanpreetsohi11@gmail.com <harmanpreetsohi11@gmail.com>
2023-06-23 19:41:01 +0530

    style changing of  quote tab

--- elements/quoteDetails.php
+++ elements/quoteDetails.php
@@ -9,210 +9,219 @@
             <button class="btn btn-primary ">View Page</button>
         </div>
     </div>
-    <!-- == -->
-    <!-- <div class="addQuote_wrapper">
-        <button class="btn btn-primary">
-            <i class="fa fa-bars" aria-hidden="true"></i>
-            Silicone Coating
-            <i class="fa fa-times" aria-hidden="true"></i>
-        </button>
-        <button class="btn btn-primary">
-            <i class="fa fa-bars" aria-hidden="true"></i>
-            new quote
-            <i class="fa fa-times" aria-hidden="true"></i>
-        </button>
-        <i class="fa fa-plus" aria-hidden="true"></i>
-    </div> -->
     <!-- new quote -->
     <div class="addQuote_wrapper">
         <ul class="nav nav-tabs" id="myTab" role="tablist">
             <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button"
-                    role="tab" aria-controls="home" aria-selected="true">
+                <button class="nav-link active" id="Quote1-tab" data-bs-toggle="tab" data-bs-target="#Quote1" type="button"
+                    role="tab" aria-controls="Quote1" aria-selected="true">
                     <i class="fa fa-bars" aria-hidden="true"></i>Silicone Coating
                     <i class="fa fa-times" aria-hidden="true"></i>
                 </button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link btn btn-primary" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button"
-                    role="tab" aria-controls="profile" aria-selected="false">
+                <button class="nav-link btn btn-primary" id="Quote2-tab" data-bs-toggle="tab" data-bs-target="#Quote2"
+                    type="button" role="tab" aria-controls="Quote2" aria-selected="false">
                     <i class="fa fa-bars" aria-hidden="true"></i>New quote
                     <i class="fa fa-times" aria-hidden="true"></i>
                 </button>
             </li>
-            
+
         </ul>
 
     </div>
+    <!-- tab `1 -->
     <div class="tab-content" id="myTabContent">
-        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-         <div class="card">
-        <div class="card-body">
-            <div class="row">
-                <div class=" col-lg-4 col-md-6 col-sm-12">
-                <label>Include all items from</label>
-                    <select class="form-select">
-                        <option>1</option>
-                        <option>1</option>
-                    </select>
-                </div>
-                <div class="col-lg-8 col-md-6 col-sm-12">
-                    <label>Description</label>
-                    <textarea class="w-100"> </textarea>
-        </div>
-
-            </div>
-        </div>
-        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">3</div>
-        
-    </div>
-</div>
-</div>
-<!-- section -->
-<div class="quote_section">
-    <div class="card">
-        <div class="card-body">
-            <!-- quote section -->
-            <div class="Quote_section_innerContent">
-                <div class="row">
-                    <div class=" col-xl-9 col-lg-8 col-sm-12 col-md-6 ">
-                        <label> Section title <i class="fa fa-caret-down" aria-hidden="true"></i></label>
-                        <input type="text" class="form-control" />
-                    </div>
-                    <div class=" col-xl-3 col-lg-4 col-sm-12 col-md-6 ">
-                        <div class="quote_switch_outer">
-                            <i class="fa fa-trash-o" aria-hidden="true"></i>
-                            <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" role="switch"
-                                    id="flexSwitchCheckDefault">
-                            </div>
+        <div class="tab-pane fade show active" id="Quote1" role="tabpanel" aria-labelledby="Quote1-tab">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class=" col-lg-4 col-md-6 col-sm-12">
+                            <label>Include all items from</label>
+                            <select class="form-select">
+                                <option>1</option>
+                                <option>1</option>
+                            </select>
+                        </div>
+                        <div class="col-lg-8 col-md-6 col-sm-12">
+                            <label>Description</label>
+                            <textarea class="w-100"> </textarea>
                         </div>
 
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+        <!--  tab 2 -->
+        <div class="tab-pane fade " id="Quote2" role="tabpanel" aria-labelledby="Quote2-tab">
+        <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class=" col-lg-4 col-md-6 col-sm-12">
+                            <label>Include all items from</label>
+                            <select class="form-select">
+                                <option>1</option>
+                                <option>1</option>
+                            </select>
+                        </div>
+                        <div class="col-lg-8 col-md-6 col-sm-12">
+                            <label>Description</label>
+                            <textarea class="w-100"> </textarea>
+                        </div>
 
                     </div>
                 </div>
-                <!-- item -->
-                <div class="row mt-4">
-                    <div class="col-lg-6">
-                        <div class="item_outer">
-                            <label>Item</label>
-                            <div class="item_content">
-                                <i class="fa fa-bars" aria-hidden="true"></i>
-                                <div class="textArea_outer">
-                                    <textarea col="100" row="20" class="textArea"></textarea>
-                                    <i class="fa fa-plus plus_icon" aria-hidden="true" data-bs-toggle="modal"
-                                        data-bs-target="#add_item"></i>
+                
+            </div>
+        </div>
+    </div>
+    <!-- section -->
+    <div class="quote_section">
+        <div class="card">
+            <div class="card-body">
+                <!-- quote section -->
+                <div class="Quote_section_innerContent">
+                    <div class="row">
+                        <div class=" col-xl-9 col-lg-8 col-sm-12 col-md-6 ">
+                            <label> Section title <i class="fa fa-caret-down" aria-hidden="true"></i></label>
+                            <input type="text" class="form-control" />
+                        </div>
+                        <div class=" col-xl-3 col-lg-4 col-sm-12 col-md-6 ">
+                            <div class="quote_switch_outer">
+                                <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" role="switch"
+                                        id="flexSwitchCheckDefault">
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                    <!-- item -->
+                    <div class="row mt-4">
+                        <div class="col-lg-6">
+                            <div class="item_outer">
+                                <label>Item</label>
+                                <div class="item_content">
+                                    <i class="fa fa-bars" aria-hidden="true"></i>
+                                    <div class="textArea_outer">
+                                        <textarea col="100" row="20" class="textArea"></textarea>
+                                        <i class="fa fa-plus plus_icon" aria-hidden="true" data-bs-toggle="modal"
+                                            data-bs-target="#add_item"></i>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-12 col-sm-12 ">
+                            <label>Quantity</label>
+                            <input class="form-control" type="number" />
+                        </div>
+                        <div class="col-lg-2 col-md-12 col-sm-12 ">
+                            <label>Price</label>
+                            <input class="form-control" type="number" />
+                        </div>
+                        <div class="col-lg-2 col-md-12 col-sm-12 ">
+                            <label>Line total</label>
+
+                            <i class="fa fa-trash-o" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <div class="section_total_btn_outer">
+                        <button class="btn btn-primary">
+                            Add item
+                        </button>
+                        <h5>
+                            section total: $0.0
+                        </h5>
+                    </div>
+                </div>
+                <!-- another qoute section -->
+                <div class="Quote_section_innerContent">
+                    <div class="row">
+                        <div class=" col-xl-9 col-lg-8 col-sm-12 col-md-6 ">
+                            <label> Section title <i class="fa fa-caret-down" aria-hidden="true"></i></label>
+                            <input type="text" class="form-control" />
+                        </div>
+                        <div class=" col-xl-3 col-lg-4 col-sm-12 col-md-6 ">
+                            <div class="quote_switch_outer">
+                                <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" role="switch"
+                                        id="flexSwitchCheckDefault">
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                    <!-- item -->
+                    <div class="row mt-4">
+                        <div class="col-lg-6">
+                            <div class="item_outer">
+                                <label>Item</label>
+                                <div class="item_content">
+                                    <i class="fa fa-bars" aria-hidden="true"></i>
+                                    <div class="textArea_outer">
+                                        <textarea col="100" row="20" class="textArea"></textarea>
+                                        <i class="fa fa-plus plus_icon" aria-hidden="true"></i>
+
+                                    </div>
 
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-2 col-md-12 col-sm-12 ">
-                        <label>Quantity</label>
-                        <input class="form-control" type="number" />
-                    </div>
-                    <div class="col-lg-2 col-md-12 col-sm-12 ">
-                        <label>Price</label>
-                        <input class="form-control" type="number" />
-                    </div>
-                    <div class="col-lg-2 col-md-12 col-sm-12 ">
-                        <label>Line total</label>
+                        <div class="col-lg-2 col-md-12 col-sm-12 ">
+                            <label>Quantity</label>
+                            <input class="form-control" type="number" />
+                        </div>
+                        <div class="col-lg-2 col-md-12 col-sm-12 ">
+                            <label>Price</label>
+                            <input class="form-control" type="number" />
+                        </div>
+                        <div class="col-lg-2 col-md-12 col-sm-12 ">
+                            <label>Line total</label>
 
-                        <i class="fa fa-trash-o" aria-hidden="true"></i>
-                    </div>
-                </div>
-                <div class="section_total_btn_outer">
-                    <button class="btn btn-primary">
-                        Add item
-                    </button>
-                    <h5>
-                        section total: $0.0
-                    </h5>
-                </div>
-            </div>
-            <!-- another qoute section -->
-            <div class="Quote_section_innerContent">
-                <div class="row">
-                    <div class=" col-xl-9 col-lg-8 col-sm-12 col-md-6 ">
-                        <label> Section title <i class="fa fa-caret-down" aria-hidden="true"></i></label>
-                        <input type="text" class="form-control" />
-                    </div>
-                    <div class=" col-xl-3 col-lg-4 col-sm-12 col-md-6 ">
-                        <div class="quote_switch_outer">
                             <i class="fa fa-trash-o" aria-hidden="true"></i>
-                            <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" role="switch"
-                                    id="flexSwitchCheckDefault">
-                            </div>
-                        </div>
-
-
-                    </div>
-                </div>
-                <!-- item -->
-                <div class="row mt-4">
-                    <div class="col-lg-6">
-                        <div class="item_outer">
-                            <label>Item</label>
-                            <div class="item_content">
-                                <i class="fa fa-bars" aria-hidden="true"></i>
-                                <div class="textArea_outer">
-                                    <textarea col="100" row="20" class="textArea"></textarea>
-                                    <i class="fa fa-plus plus_icon" aria-hidden="true"></i>
-
-                                </div>
-
-                            </div>
                         </div>
                     </div>
-                    <div class="col-lg-2 col-md-12 col-sm-12 ">
-                        <label>Quantity</label>
-                        <input class="form-control" type="number" />
-                    </div>
-                    <div class="col-lg-2 col-md-12 col-sm-12 ">
-                        <label>Price</label>
-                        <input class="form-control" type="number" />
-                    </div>
-                    <div class="col-lg-2 col-md-12 col-sm-12 ">
-                        <label>Line total</label>
-
-                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                    <div class="section_total_btn_outer">
+                        <button class="btn btn-primary">
+                            Add item
+                        </button>
+                        <h5>
+                            section total: $0.0
+                        </h5>
                     </div>
                 </div>
-                <div class="section_total_btn_outer">
-                    <button class="btn btn-primary">
-                        Add item
-                    </button>
-                    <h5>
-                        section total: $0.0
-                    </h5>
+                <div class="quote_add_section_btn">
+                    <button class="btn btn-primary">Add section</button>
+
                 </div>
-            </div>
-            <div class="quote_add_section_btn">
-                <button class="btn btn-primary">Add section</button>
+                <div class="row">
 
-            </div>
-            <div class="row">
-
-                <div class="col-lg-6 col-md-12 col-sm-12 ms-auto">
-                    <div class="total_outer">
-                        <h6>
-                            Quote subtotal</h6>
+                    <div class="col-lg-6 col-md-12 col-sm-12 ms-auto">
+                        <div class="total_outer">
+                            <h6>
+                                Quote subtotal</h6>
 
 
-                        <h5 class=" mt-2">
-                            Total</h5>
+                            <h5 class=" mt-2">
+                                Total</h5>
 
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="notes_wrapper">
-                <h5 class="pt-3">Notes</h5>
-                <textarea class="text_area"></textarea>
+                <div class="notes_wrapper">
+                    <h5 class="pt-3">Notes</h5>
+                    <textarea class="text_area"></textarea>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </div>
 <!-- quote add item modal -->
 <!-- Modal -->
